411e31fecdc075ef0b2087222205680466f212c9
Andrey Shumilov <sumilovirk@gmail.com>
2021-03-20 17:01:59 +0800

    KVAZI-8 Написать builder для создания типа задачи ЛП.

--- core/models.py
+++ core/models.py
@@ -1,5 +1,6 @@
-from dto import Data
-from pulp import LpVariable
+from core.dto import Data, TaskType
+from pulp import LpVariable, LpProblem
+from pulp.constants import LpMinimize, LpMaximize
 
 
 class LpSolve(object):
@@ -7,7 +8,7 @@ class LpSolve(object):
         self.c = None
         self.x = None
         self.vars = self._build_vars(len(data.c))
-        self.problem = None
+        self.problem = self._build_problems(data.type)
 
     @staticmethod
     def _build_vars(count_var: int) -> dict:
@@ -17,6 +18,14 @@ class LpSolve(object):
             _vars.setdefault(var_name, LpVariable(var_name, lowBound=0))
 
         return _vars
+
+    @staticmethod
+    def _build_problems(_type: TaskType) -> LpProblem:
+        if _type == TaskType.MIN:
+            return LpProblem('Задача_ЛП', LpMinimize)
+        if _type == TaskType.MAX:
+            return LpProblem('Задача_ЛП', LpMaximize)
+        raise TypeError
 
     def run(self):
         self.problem.solve()
