9cd2696434053fbbc102860cd9c21b62caa3a065
Andrey Shumilov <sumilovirk@gmail.com>
2021-03-20 16:39:27 +0800

    KVAZI-7 Создать builder для переменных.

--- core/models.py
+++ core/models.py
@@ -1,12 +1,22 @@
 from dto import Data
+from pulp import LpVariable
 
 
 class LpSolve(object):
     def __init__(self, data: Data):
         self.c = None
         self.x = None
-        self.vars = None
+        self.vars = self._build_vars(len(data.c))
         self.problem = None
+
+    @staticmethod
+    def _build_vars(count_var: int) -> dict:
+        _vars = dict()
+        for index in range(count_var):
+            var_name = 'x{}'.format(index)
+            _vars.setdefault(var_name, LpVariable(var_name, lowBound=0))
+
+        return _vars
 
     def run(self):
         self.problem.solve()
