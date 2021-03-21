f3c8c0f6ab1a447fba9e63089720b5424a3cb3c5
Andrey Shumilov <sumilovirk@gmail.com>
2021-03-21 13:47:22 +0800

    KVAZI-9 Написать builder для создания целевой функции.

--- test/core/test_models.py
+++ test/core/test_models.py
@@ -1,7 +1,22 @@
 import unittest
 from core.models import LpSolve
-from core.dto import TaskType
+from core.dto import TaskType, Data
 from pulp.constants import LpMaximize, LpMinimize
+from pulp import LpVariable
+
+
+def get_expect_data():
+    params = {
+        "type": "MIN",
+        "c": [1, 2, -3, 2],
+        "restrictions": [
+            [1, 2, 4, 5],
+            [0, 4, -4, 0]
+        ],
+        "comparisonOperators": ["<", "="],
+        "b": [2, 3]
+    }
+    return Data(params)
 
 
 class TestBuildLpSolve(unittest.TestCase):
@@ -18,6 +33,23 @@ class TestBuildLpSolve(unittest.TestCase):
         self.assertEqual(actual.sense, LpMinimize)
         self.assertRaises(TypeError, LpSolve._build_problems, None)
         self.assertRaises(TypeError, LpSolve._build_problems, 1)
+
+    def test_get_var(self):
+        actual = LpSolve(get_expect_data())
+        expect = LpVariable('x1', lowBound=0)
+        self.assertEqual(actual.get_var(1).toDict(), expect.toDict())
+        expect = LpVariable('x2', lowBound=0)
+        self.assertEqual(actual.get_var(2).toDict(), expect.toDict())
+        self.assertEqual(actual.get_var(5), None)
+
+    def test_build_problem_c(self):
+        actual = LpSolve(get_expect_data())
+        self.assertEqual(actual.problem.objective.name, 'Целевая_функция')
+        self.assertEqual(actual.problem.objective.toDict(),
+              [{'name': 'x1', 'value': 1},
+               {'name': 'x2', 'value': 2},
+               {'name': 'x3', 'value': -3},
+               {'name': 'x4', 'value': 2}])
 
 
 if __name__ == '__main__':
